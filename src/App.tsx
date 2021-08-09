b31866cd0cbb660ee686f9c38d2566ccb314b744
Jun Shindo <46585162+jay-es@users.noreply.github.com>
2021-08-10 00:00:36 +0900

    カウント追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Component, For } from "solid-js";
+import { batch, Component, For } from "solid-js";
 import styles from "./App.module.css";
 import { useStore } from "./store";
 import { TodoForm } from "./TodoForm";
@@ -6,19 +6,27 @@ import { TodoItem } from "./TodoItem";
 
 const App: Component = () => {
   const { state, addTodo, toggleTodo } = useStore();
+  const totalCount = () => state.todos.length;
+  const undoneCount = () => state.todos.filter((v) => !v.done).length;
 
   // リストが空の状態を確認するため、処理を遅らせる
   setTimeout(() => {
-    if (state.todos.length) return;
-    addTodo("task1");
-    addTodo("task2");
-    addTodo("task3");
-    toggleTodo(1);
+    if (totalCount()) return;
+
+    batch(() => {
+      addTodo("task1");
+      addTodo("task2");
+      addTodo("task3");
+      toggleTodo(1);
+    });
   }, 400);
 
   return (
     <div class={styles.App}>
       <TodoForm />
+      <p>
+        {totalCount} items ({undoneCount} undone)
+      </p>
       <ul class={styles.list}>
         <For each={state.todos} fallback={<p>No Tasks</p>}>
           {(todo) => <TodoItem {...todo} />}
